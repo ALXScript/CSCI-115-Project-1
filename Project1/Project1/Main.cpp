33d80dfbf6d7f67daf63604e5700f99bb7597ed1
Alex <alexiscontrerase9@gmail.com>
2018-03-18 20:18:02 -0700

    Main menu not working

--- Project1/Project1/Main.cpp
+++ Project1/Project1/Main.cpp
@@ -7,12 +7,11 @@ using namespace std;
 void main()
 {	
 	//characters for selecting the types we want
-	char sort;
-	char arrayType;
-	int arraySize;
+	bool exit = false;
+	char sort = 'z';
+	char arrayType = 'z';
+	int arraySize = 0;
 
-	
-	
 	/*
 	legend:
 	e = Exit
@@ -23,10 +22,15 @@ void main()
 	m = Merge Sort
 	*/
 	
-	while (sort != 'e')
+	do
 	{
 		switch (sort)
 		{
+		//Exit
+		case 'e':
+			exit = true;
+			break;
+
 		//Insertion sort
 		case 'i':
 			switch (arrayType)
@@ -37,28 +41,7 @@ void main()
 				{
 				//size 10
 				case 10:
-					/*
 					//Insertion Sort for a Sorted Array of Size 10
-					ourArray<double> timeArray(100);
-					
-					for(int i = 0, i < 100, i++)
-					{
-						ourArray<int> execute(arraySize)
-						execute.preSort();
-						Start timer
-						insSort(execute);
-						end timer
-						double timeSpent = end timer - start timer
-						timeArray.setElem(i, timeSpent);
-						~execute()
-					}
-
-					cout << endl << "Mean of Insertion Sort with Sorted Array of Size " << arraySize << " = " << timeArray.getMean() << endl;
-					cout << endl << "Standard Deviation of Insertion Sort with Sorted Array of Size " << arraySize << " = " << timeArray.getStDev() << endl;
-
-					~timeArray();
-					
-					*/
 					break;
 
 				//size 1000
@@ -83,42 +66,764 @@ void main()
 
 				//default UI
 				default:
+					cin.ignore();
+					cout << "\n\nPlease Select the amount of elements you would like to sort:\n10\t1000\t10000\n100000\t1000000\n";
+					cin >> arraySize;
 					break;
 				}
 				break;
 				
 			//completely shuffled
 			case 'h':
+				switch (arraySize)
+				{
+				//size 10
+				case 10:
+					//Insertion Sort for a Completely Shuffled Array of Size 10
+					break;
+
+				//size 1000
+				case 1000:
+					//Insertion Sort For a Completely Shuffled Array of Size 1,000
+					break;
+
+				//size 10,000
+				case 10000:
+					//Insertion Sort For a Completely Shuffled Array of Size 10,000
+					break;
+
+				//size 100,000
+				case 100000:
+					//Insertion Sort For a Completely Shuffled Array of Size 100,000
+					break;
+
+				//size 1,000,000
+				case 1000000:
+					//Insertion Sort For a Completely Shuffled Array of Size 1,000,000
+					break;
+
+				//default UI
+				default:
+					cout << "\n\nPlease Select the amount of elements you would like to sort:\n10\t1000\t10000\n100000\t1000000\n";
+					cin >> arraySize;
+					break;
+				}
 				break;
 
 			//reverse order
 			case 'r':
+				switch (arraySize)
+				{
+				//size 10
+				case 10:
+					//Insertion Sort for a Reverse Order Array of Size 10
+					break;
+
+				//size 1000
+				case 1000:
+					//Insertion Sort For a Reverse Order Array of Size 1,000
+					break;
+
+				//size 10,000
+				case 10000:
+					//Insertion Sort For a Reverse Order Array of Size 10,000
+					break;
+
+				//size 100,000
+				case 100000:
+					//Insertion Sort For a Reverse Order Array of Size 100,000
+					break;
+
+				//size 1,000,000
+				case 1000000:
+					//Insertion Sort For a Reverse Order Array of Size 1,000,000
+					break;
+
+				//default UI
+				default:
+					cout << "\n\nPlease Select the amount of elements you would like to sort:\n10\t1000\t10000\n100000\t1000000\n";
+					cin >> arraySize;
+					break;
+				}
 				break;
 
 			//sorted and shuffled at 10%
 			case 't':
+				switch (arraySize)
+				{
+				//size 10
+				case 10:
+					//Insertion Sort for a Sorted and Shuffled at 10% Array of Size 10
+					break;
+
+				//size 1000
+				case 1000:
+					//Insertion Sort For a Sorted and Shuffled at 10% Array of Size 1,000
+					break;
+
+				//size 10,000
+				case 10000:
+					//Insertion Sort For a Sorted and Shuffled at 10% Array of Size 10,000
+					break;
+
+				//size 100,000
+				case 100000:
+					//Insertion Sort For a Sorted and Shuffled at 10% Array of Size 100,000
+					break;
+
+				//size 1,000,000
+				case 1000000:
+					//Insertion Sort For a Sorted and Shuffled at 10% Array of Size 1,000,000
+					break;
+
+				//default UI
+				default:
+					cout << "\n\nPlease Select the amount of elements you would like to sort:\n10\t1000\t10000\n100000\t1000000\n";
+					cin >> arraySize;
+					break;
+				}
 				break;
 
 			//default UI
-			default:
+			default: cout << "\n\nPlease select the type of array you would like:\ns: Already Sorted\th: Completely Shuffled\nr: Sorted in Reverse Order\tt: Sorted and Shuffled at 10%\n";
 				break;
 			}
 			break;
 
 		//Selection sort
 		case 's':
+			cin.clear();
+			switch (arrayType)
+			{
+				//sorted
+			case 's':
+				switch (arraySize)
+				{
+					//size 10
+				case 10:
+					//Selection Sort for a Sorted Array of Size 10
+					break;
+
+					//size 1000
+				case 1000:
+					//Selection Sort For a Sorted Array of Size 1,000
+					break;
+
+					//size 10,000
+				case 10000:
+					//Selection Sort For a Sorted Array of Size 10,000
+					break;
+
+					//size 100,000
+				case 100000:
+					//Selection Sort For a Sorted Array of Size 100,000
+					break;
+
+					//size 1,000,000
+				case 1000000:
+					//Selection Sort For a Sorted Array of Size 1,000,000
+					break;
+
+					//default UI
+				default:
+					cout << "\n\nPlease Select the amount of elements you would like to sort:\n10\t1000\t10000\n100000\t1000000\n";
+					cin >> arraySize;
+					break;
+				}
+				break;
+
+				//completely shuffled
+			case 'h':
+				switch (arraySize)
+				{
+					//size 10
+				case 10:
+					//Selection Sort for a Completely Shuffled Array of Size 10
+					break;
+
+					//size 1000
+				case 1000:
+					//Selection Sort For a Completely Shuffled Array of Size 1,000
+					break;
+
+					//size 10,000
+				case 10000:
+					//Selection Sort For a Completely Shuffled Array of Size 10,000
+					break;
+
+					//size 100,000
+				case 100000:
+					//Selection Sort For a Completely Shuffled Array of Size 100,000
+					break;
+
+					//size 1,000,000
+				case 1000000:
+					//Selection Sort For a Completely Shuffled Array of Size 1,000,000
+					break;
+
+					//default UI
+				default:
+					cout << "\n\nPlease Select the amount of elements you would like to sort:\n10\t1000\t10000\n100000\t1000000\n";
+					cin >> arraySize;
+					break;
+				}
+				break;
+
+				//reverse order
+			case 'r':
+				switch (arraySize)
+				{
+					//size 10
+				case 10:
+					//Selection Sort for a Reverse Order Array of Size 10
+					break;
+
+					//size 1000
+				case 1000:
+					//Selection Sort For a Reverse Order Array of Size 1,000
+					break;
+
+					//size 10,000
+				case 10000:
+					//Selection Sort For a Reverse Order Array of Size 10,000
+					break;
+
+					//size 100,000
+				case 100000:
+					//Selection Sort For a Reverse Order Array of Size 100,000
+					break;
+
+					//size 1,000,000
+				case 1000000:
+					//Selection Sort For a Reverse Order Array of Size 1,000,000
+					break;
+
+					//default UI
+				default:
+					cout << "\n\nPlease Select the amount of elements you would like to sort:\n10\t1000\t10000\n100000\t1000000\n";
+					cin >> arraySize;
+					break;
+				}
+				break;
+
+				//sorted and shuffled at 10%
+			case 't':
+				switch (arraySize)
+				{
+					//size 10
+				case 10:
+					//Selection Sort for a Sorted and Shuffled at 10% Array of Size 10
+					break;
+
+					//size 1000
+				case 1000:
+					//Selection Sort For a Sorted and Shuffled at 10% Array of Size 1,000
+					break;
+
+					//size 10,000
+				case 10000:
+					//Selection Sort For a Sorted and Shuffled at 10% Array of Size 10,000
+					break;
+
+					//size 100,000
+				case 100000:
+					//Selection Sort For a Sorted and Shuffled at 10% Array of Size 100,000
+					break;
+
+					//size 1,000,000
+				case 1000000:
+					//Selection Sort For a Sorted and Shuffled at 10% Array of Size 1,000,000
+					break;
+
+					//default UI
+				default:
+					cout << "\n\nPlease Select the amount of elements you would like to sort:\n10\t1000\t10000\n100000\t1000000\n";
+					cin >> arraySize;
+					break;
+				}
+				break;
+
+				//default UI
+			default: cout << "\n\nPlease select the type of array you would like:\ns: Already Sorted\th: Completely Shuffled\nr: Sorted in Reverse Order\tt: Sorted and Shuffled at 10%\n";
+				break;
+			}
 			break;
 
 		//Bubble Sort
 		case 'b':
+			cin.clear();
+			switch (arrayType)
+			{
+				//sorted
+			case 's':
+				switch (arraySize)
+				{
+					//size 10
+				case 10:
+					//Bubble Sort for a Sorted Array of Size 10
+					break;
+
+					//size 1000
+				case 1000:
+					//Bubble Sort For a Sorted Array of Size 1,000
+					break;
+
+					//size 10,000
+				case 10000:
+					//Bubble Sort For a Sorted Array of Size 10,000
+					break;
+
+					//size 100,000
+				case 100000:
+					//Bubble Sort For a Sorted Array of Size 100,000
+					break;
+
+					//size 1,000,000
+				case 1000000:
+					//Bubble Sort For a Sorted Array of Size 1,000,000
+					break;
+
+					//default UI
+				default:
+					cout << "\n\nPlease Select the amount of elements you would like to sort:\n10\t1000\t10000\n100000\t1000000\n";
+					cin >> arraySize;
+					break;
+				}
+				break;
+
+				//completely shuffled
+			case 'h':
+				switch (arraySize)
+				{
+					//size 10
+				case 10:
+					//Bubble Sort for a Completely Shuffled Array of Size 10
+					break;
+
+					//size 1000
+				case 1000:
+					//Bubble Sort For a Completely Shuffled Array of Size 1,000
+					break;
+
+					//size 10,000
+				case 10000:
+					//Bubble Sort For a Completely Shuffled Array of Size 10,000
+					break;
+
+					//size 100,000
+				case 100000:
+					//Bubble Sort For a Completely Shuffled Array of Size 100,000
+					break;
+
+					//size 1,000,000
+				case 1000000:
+					//Bubble Sort For a Completely Shuffled Array of Size 1,000,000
+					break;
+
+					//default UI
+				default:
+					cout << "\n\nPlease Select the amount of elements you would like to sort:\n10\t1000\t10000\n100000\t1000000\n";
+					cin >> arraySize;
+					break;
+				}
+				break;
+
+				//reverse order
+			case 'r':
+				switch (arraySize)
+				{
+					//size 10
+				case 10:
+					//Bubble Sort for a Reverse Order Array of Size 10
+					break;
+
+					//size 1000
+				case 1000:
+					//Bubble Sort For a Reverse Order Array of Size 1,000
+					break;
+
+					//size 10,000
+				case 10000:
+					//Bubble Sort For a Reverse Order Array of Size 10,000
+					break;
+
+					//size 100,000
+				case 100000:
+					//Bubble Sort For a Reverse Order Array of Size 100,000
+					break;
+
+					//size 1,000,000
+				case 1000000:
+					//Bubble Sort For a Reverse Order Array of Size 1,000,000
+					break;
+
+					//default UI
+				default:
+					cout << "\n\nPlease Select the amount of elements you would like to sort:\n10\t1000\t10000\n100000\t1000000\n";
+					cin >> arraySize;
+					break;
+				}
+				break;
+
+				//sorted and shuffled at 10%
+			case 't':
+				switch (arraySize)
+				{
+					//size 10
+				case 10:
+					//Bubble Sort for a Sorted and Shuffled at 10% Array of Size 10
+					break;
+
+					//size 1000
+				case 1000:
+					//Bubble Sort For a Sorted and Shuffled at 10% Array of Size 1,000
+					break;
+
+					//size 10,000
+				case 10000:
+					//Bubble Sort For a Sorted and Shuffled at 10% Array of Size 10,000
+					break;
+
+					//size 100,000
+				case 100000:
+					//Bubble Sort For a Sorted and Shuffled at 10% Array of Size 100,000
+					break;
+
+					//size 1,000,000
+				case 1000000:
+					//Bubble Sort For a Sorted and Shuffled at 10% Array of Size 1,000,000
+					break;
+
+					//default UI
+				default:
+					cout << "\n\nPlease Select the amount of elements you would like to sort:\n10\t1000\t10000\n100000\t1000000\n";
+					cin >> arraySize;
+					break;
+				}
+				break;
+
+				//default UI
+			default: cout << "\n\nPlease select the type of array you would like:\ns: Already Sorted\th: Completely Shuffled\nr: Sorted in Reverse Order\tt: Sorted and Shuffled at 10%\n";
+				break;
+			}
 			break;
 
 		//Quick Sort
 		case 'q':
+			cin.clear();
+			switch (arrayType)
+			{
+				//sorted
+			case 's':
+				switch (arraySize)
+				{
+					//size 10
+				case 10:
+					//Quick Sort for a Sorted Array of Size 10
+					break;
+
+					//size 1000
+				case 1000:
+					//Quick Sort For a Sorted Array of Size 1,000
+					break;
+
+					//size 10,000
+				case 10000:
+					//Quick Sort For a Sorted Array of Size 10,000
+					break;
+
+					//size 100,000
+				case 100000:
+					//Quick Sort For a Sorted Array of Size 100,000
+					break;
+
+					//size 1,000,000
+				case 1000000:
+					//Quick Sort For a Sorted Array of Size 1,000,000
+					break;
+
+					//default UI
+				default:
+					cout << "\n\nPlease Select the amount of elements you would like to sort:\n10\t1000\t10000\n100000\t1000000\n";
+					cin >> arraySize;
+					break;
+				}
+				break;
+
+				//completely shuffled
+			case 'h':
+				switch (arraySize)
+				{
+					//size 10
+				case 10:
+					//Quick Sort for a Completely Shuffled Array of Size 10
+					break;
+
+					//size 1000
+				case 1000:
+					//Quick Sort For a Completely Shuffled Array of Size 1,000
+					break;
+
+					//size 10,000
+				case 10000:
+					//Quick Sort For a Completely Shuffled Array of Size 10,000
+					break;
+
+					//size 100,000
+				case 100000:
+					//Quick Sort For a Completely Shuffled Array of Size 100,000
+					break;
+
+					//size 1,000,000
+				case 1000000:
+					//Quick Sort For a Completely Shuffled Array of Size 1,000,000
+					break;
+
+					//default UI
+				default:
+					cout << "\n\nPlease Select the amount of elements you would like to sort:\n10\t1000\t10000\n100000\t1000000\n";
+					cin >> arraySize;
+					break;
+				}
+				break;
+
+				//reverse order
+			case 'r':
+				switch (arraySize)
+				{
+					//size 10
+				case 10:
+					//Quick Sort for a Reverse Order Array of Size 10
+					break;
+
+					//size 1000
+				case 1000:
+					//Quick Sort For a Reverse Order Array of Size 1,000
+					break;
+
+					//size 10,000
+				case 10000:
+					//Quick Sort For a Reverse Order Array of Size 10,000
+					break;
+
+					//size 100,000
+				case 100000:
+					//Quick Sort For a Reverse Order Array of Size 100,000
+					break;
+
+					//size 1,000,000
+				case 1000000:
+					//Quick Sort For a Reverse Order Array of Size 1,000,000
+					break;
+
+					//default UI
+				default:
+					cout << "\n\nPlease Select the amount of elements you would like to sort:\n10\t1000\t10000\n100000\t1000000\n";
+					cin >> arraySize;
+					break;
+				}
+				break;
+
+				//sorted and shuffled at 10%
+			case 't':
+				switch (arraySize)
+				{
+					//size 10
+				case 10:
+					//Quick Sort for a Sorted and Shuffled at 10% Array of Size 10
+					break;
+
+					//size 1000
+				case 1000:
+					//Quick Sort For a Sorted and Shuffled at 10% Array of Size 1,000
+					break;
+
+					//size 10,000
+				case 10000:
+					//Quick Sort For a Sorted and Shuffled at 10% Array of Size 10,000
+					break;
+
+					//size 100,000
+				case 100000:
+					//Quick Sort For a Sorted and Shuffled at 10% Array of Size 100,000
+					break;
+
+					//size 1,000,000
+				case 1000000:
+					//Quick Sort For a Sorted and Shuffled at 10% Array of Size 1,000,000
+					break;
+
+					//default UI
+				default:
+					cout << "\n\nPlease Select the amount of elements you would like to sort:\n10\t1000\t10000\n100000\t1000000\n";
+					cin >> arraySize;
+					break;
+				}
+				break;
+
+				//default UI
+			default: cout << "\n\nPlease select the type of array you would like:\ns: Already Sorted\th: Completely Shuffled\nr: Sorted in Reverse Order\tt: Sorted and Shuffled at 10%\n";
+				break;
+			}
 			break;
 
 		//Merge Sort
 		case 'm':
+			cin.clear();
+			switch (arrayType)
+			{
+				//sorted
+			case 's':
+				switch (arraySize)
+				{
+					//size 10
+				case 10:
+					//Merge Sort for a Sorted Array of Size 10
+					break;
+
+					//size 1000
+				case 1000:
+					//Merge Sort For a Sorted Array of Size 1,000
+					break;
+
+					//size 10,000
+				case 10000:
+					//Merge Sort For a Sorted Array of Size 10,000
+					break;
+
+					//size 100,000
+				case 100000:
+					//Merge Sort For a Sorted Array of Size 100,000
+					break;
+
+					//size 1,000,000
+				case 1000000:
+					//Merge Sort For a Sorted Array of Size 1,000,000
+					break;
+
+					//default UI
+				default:
+					cout << "\n\nPlease Select the amount of elements you would like to sort:\n10\t1000\t10000\n100000\t1000000\n";
+					cin >> arraySize;
+					break;
+				}
+				break;
+
+				//completely shuffled
+			case 'h':
+				switch (arraySize)
+				{
+					//size 10
+				case 10:
+					//Merge Sort for a Completely Shuffled Array of Size 10
+					break;
+
+					//size 1000
+				case 1000:
+					//Merge Sort For a Completely Shuffled Array of Size 1,000
+					break;
+
+					//size 10,000
+				case 10000:
+					//Merge Sort For a Completely Shuffled Array of Size 10,000
+					break;
+
+					//size 100,000
+				case 100000:
+					//Merge Sort For a Completely Shuffled Array of Size 100,000
+					break;
+
+					//size 1,000,000
+				case 1000000:
+					//Merge Sort For a Completely Shuffled Array of Size 1,000,000
+					break;
+
+					//default UI
+				default:
+					cout << "\n\nPlease Select the amount of elements you would like to sort:\n10\t1000\t10000\n100000\t1000000\n";
+					cin >> arraySize;
+					break;
+				}
+				break;
+
+				//reverse order
+			case 'r':
+				switch (arraySize)
+				{
+					//size 10
+				case 10:
+					//Merge Sort for a Reverse Order Array of Size 10
+					break;
+
+					//size 1000
+				case 1000:
+					//Merge Sort For a Reverse Order Array of Size 1,000
+					break;
+
+					//size 10,000
+				case 10000:
+					//Merge Sort For a Reverse Order Array of Size 10,000
+					break;
+
+					//size 100,000
+				case 100000:
+					//Merge Sort For a Reverse Order Array of Size 100,000
+					break;
+
+					//size 1,000,000
+				case 1000000:
+					//Merge Sort For a Reverse Order Array of Size 1,000,000
+					break;
+
+					//default UI
+				default:
+					cout << "\n\nPlease Select the amount of elements you would like to sort:\n10\t1000\t10000\n100000\t1000000\n";
+					cin >> arraySize;
+					break;
+				}
+				break;
+
+				//sorted and shuffled at 10%
+			case 't':
+				switch (arraySize)
+				{
+					//size 10
+				case 10:
+					//Merge Sort for a Sorted and Shuffled at 10% Array of Size 10
+					break;
+
+					//size 1000
+				case 1000:
+					//Merge Sort For a Sorted and Shuffled at 10% Array of Size 1,000
+					break;
+
+					//size 10,000
+				case 10000:
+					//Merge Sort For a Sorted and Shuffled at 10% Array of Size 10,000
+					break;
+
+					//size 100,000
+				case 100000:
+					//Merge Sort For a Sorted and Shuffled at 10% Array of Size 100,000
+					break;
+
+					//size 1,000,000
+				case 1000000:
+					//Merge Sort For a Sorted and Shuffled at 10% Array of Size 1,000,000
+					break;
+
+					//default UI
+				default:
+					cout << "\n\nPlease Select the amount of elements you would like to sort:\n10\t1000\t10000\n100000\t1000000\n";
+					cin >> arraySize;
+					break;
+				}
+				break;
+
+				//default UI
+			default: cout << "\n\nPlease select the type of array you would like:\ns: Already Sorted\th: Completely Shuffled\nr: Sorted in Reverse Order\tt: Sorted and Shuffled at 10%\n";
+				break;
+			}
 			break;
 
 		//default UI
@@ -127,7 +832,6 @@ void main()
 			cin >> sort;
 			break;
 		}
-	}
-	
+	} while (!exit);
 	
 }
